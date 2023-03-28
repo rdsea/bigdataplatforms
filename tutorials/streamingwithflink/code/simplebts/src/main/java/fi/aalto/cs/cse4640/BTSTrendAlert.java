bda5abfdf47857194f2de078c25e76ec4d4936fc
Trí Nguyễn <tri.m.nguyen@aalto.fi>
2023-03-28 15:02:38 +0300

    update flink for Google dataproc

---
+++ tutorials/streamingwithflink/code/simplebts/src/main/java/fi/aalto/cs/cse4640/BTSTrendAlert.java
@@ -0,0 +1,24 @@
+/*
+ * CS-E4640
+ * Linh Truong
+ */
+package fi.aalto.cs.cse4640;
+public class BTSTrendAlert {
+    public String trend = "";
+    public String station_id;
+    public BTSTrendAlert() {
+
+    }
+    public BTSTrendAlert(String station_id, String trend) {
+      this.station_id = station_id;
+      this.trend =trend;
+    }
+
+    public String toString() {
+      return "Station "+station_id+" has "+trend+" trend";
+  }
+    public String toJSON() {
+    return "{\"btsalarmalert\":{\"station_id\":"+station_id+", \"trend\":"+trend+"}}";
+  }
+
+}

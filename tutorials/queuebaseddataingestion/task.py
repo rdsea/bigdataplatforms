51b1f74a36b855ea112b2ab9ae8faa6d0d7d234f
mabaoer <651540377@qq.com>
2023-10-23 16:47:52 +0800

    Add headers to prevent "<urlopen error [Errno 111] Connection refused>" and format code.

--- tutorials/queuebaseddataingestion/task.py
+++ tutorials/queuebaseddataingestion/task.py
@@ -2,35 +2,47 @@ import os
 import tempfile
 import csv
 import urllib.request
-#import urllib.request.urlretrieve
-'''
-this task assume that we get an CSV and store into a database.
+import socket
 
-However, we do not implement any database task. you can just do it.
+socket.setdefaulttimeout(5)
+
+'''
+This task assumes that we get a CSV and store it in a database.
+
+However, we do not implement a database task, you can just do it.
 
 Note that the assumption is that we must be able to get the file.
 '''
+
+
 def ingest_csv_file(url):
     '''
-    The assumpsion is to stored the file temporary in the same directory
+        It is assumed that the file is stored in a new temporary directory.
     '''
     try:
         basename = os.path.basename(url)
-        input_file=tempfile.mkdtemp()+'/'+basename
-        print("Log: ",url)
+        input_file = tempfile.mkdtemp() + '/' + basename
+        print("Log: ", url)
         print("Copy data to temp file")
-        urllib.request.urlretrieve(url,input_file)
+
+        '''
+        Add headers to prevent "<urlopen error [Errno 111] Connection refused>"
+        '''
+        opener = urllib.request.build_opener()
+        opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+        urllib.request.install_opener(opener)
+        urllib.request.urlretrieve(url, input_file)
 
         with open(input_file, encoding='utf-8') as csv_file:
             print("Process csv data")
             '''
             No store operation has been implemented. You can implement the operation with common databases.
-            You can laso just store data into files and let other components to do the storing.
+            You can also just store data into files and let other components to do the storing.
             '''
             csvReader = csv.DictReader(csv_file)
             for row in csvReader:
                 print(row)
-        return {"url":url,"result":"OK"}
+        return {"url": url, "result": "OK"}
     except Exception as err:
         print(err)
-        return {"url":url,"error":str(err)}
+        return {"url": url, "error": str(err)}
